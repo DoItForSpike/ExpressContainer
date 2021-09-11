b3c65bd375365d07d5b3081e47cd33087d986837
Kevin Peters <kwpeters@ra.rockwell.com>
2021-09-11 11:41:13 -0400

    Updated config for no-unused-vars eslint rule.

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -13,7 +13,7 @@ const { createLogicalAnd } = require("typescript");
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env["PORT"] || '3000');
 app.set('port', port);
 
 /**
